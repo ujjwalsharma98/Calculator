82ef5779f7a649b0522971a747f031e83f563b65
ujjwalsharma98 <us36335@gmail.com>
2019-04-29 11:46:47 +0530

    Calculator

---
+++ demo.js
@@ -0,0 +1,48 @@
+var zero=document.getElementById('0');
+var one=document.getElementById('1');
+var two=document.getElementById('2');
+var three=document.getElementById('3');
+var four=document.getElementById('4');
+var five=document.getElementById('5');
+var six=document.getElementById('6');
+var seven=document.getElementById('7');
+var eight=document.getElementById('8');
+var nine=document.getElementById('9');
+var dot=document.getElementById('.');
+var divide=document.getElementById('/');
+var multiply=document.getElementById('*');
+var add=document.getElementById('+');
+var subtract = document.getElementById('-');
+var modulus=document.getElementById('%');
+var result=document.getElementById('=');
+var clear=document.getElementById('clear');
+var input=document.getElementById('input');
+var change=document.getElementById('change');
+
+subtract.addEventListener('click', function(){
+
+});
+
+add.addEventListener('click', function(){
+
+});
+
+multiply.addEventListener('click', function(){
+
+});
+
+divide.addEventListener('click', function(){
+
+});
+
+modulus.addEventListener('click', function(){
+
+});
+
+clear.addEventListener('click', function(){
+
+});
+
+result.addEventListener('click', function(){
+
+});
