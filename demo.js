9c240f4d08b2a20f1834f06d00e16d0276a9891f
ujjwalsharma98 <us36335@gmail.com>
2019-05-01 01:13:22 +0530

    js & logo added

--- demo.js
+++ demo.js
@@ -18,31 +18,111 @@ var result=document.getElementById('=');
 var clear=document.getElementById('clear');
 var input=document.getElementById('input');
 var change=document.getElementById('change');
+var firstHalf = "";
+var final=0;
+var operator= '+';
+var string="";
 
 subtract.addEventListener('click', function(){
+    if(input.value != ""){
+        firstHalf = input.value;
+        input.value = "";
+        operator = subtract;
+    }else{
+        input.value+= '-';
+    }
+});
 
+zero.addEventListener('click',function(){
+	input.value +=zero.value;
+});
+
+one.addEventListener('click',function(){
+	input.value +=one.value;
+});
+
+two.addEventListener('click',function(){
+	input.value +=two.value;
+});
+
+three.addEventListener('click',function(){
+	input.value +=three.value;
+});
+
+four.addEventListener('click',function(){
+	input.value +=four.value;
+});
+
+five.addEventListener('click',function(){
+	input.value +=five.value;
+});
+
+six.addEventListener('click',function(){
+	input.value +=six.value;
+});
+
+seven.addEventListener('click',function(){
+	input.value +=seven.value;
+});
+
+eight.addEventListener('click',function(){
+	input.value +=eight.value;
+});
+
+nine.addEventListener('click',function(){
+	input.value +=nine.value;
 });
 
 add.addEventListener('click', function(){
-
+    firstHalf=input.value;
+	input.value=null;
+	operator= add;
 });
 
 multiply.addEventListener('click', function(){
-
+    firstHalf=input.value;
+	input.value=null;
+	operator= multiply;
 });
 
 divide.addEventListener('click', function(){
-
+    firstHalf=input.value;
+	input.value=null;
+	operator= divide;
 });
 
 modulus.addEventListener('click', function(){
-
+    firstHalf=input.value;
+	input.value=null;
+	operator= modulus;
 });
 
 clear.addEventListener('click', function(){
-
+    input.value=null;
 });
 
-result.addEventListener('click', function(){
+dot.addEventListener('click',function(){
+	input.value +=dot.value;
+});
 
+change.addEventListener('click',function(){
+	string=input.value;
+	var FirstChar = string.charAt(0);
+	if(FirstChar=='-'){
+		string=string.substring(1,string.length);
+		input.value=string;
+	}
+	else{
+		string='-'+string;
+		input.value=string;
+	}
+})
+
+result.addEventListener('click',function(){
+	final = eval (firstHalf+""+operator.value+""+input.value);
+	if (final=='Infinity') {
+		input.value='ERROR'
+	}else{
+	input.value=final;
+}
 });
